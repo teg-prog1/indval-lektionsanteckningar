df4994407b0db29b12b084a7ad01f9456c518962
Felicia Rosenberg <felicia.rosenberg@tabyenskilda.se>
2023-09-20 14:23:19 +0200

    genomgång klar

--- indval-Vecka-38.py
+++ indval-Vecka-38.py
@@ -3,26 +3,49 @@
 # Listor
 # Kan lagra flera värden i en variabel
 
+frukter = ["äpple","banan","mango","päron","mango"]
+
 # Listor är ordnade, det vill säga ordningen spelar roll
 # De är indexerade
+#print(frukter[1])
 
 # Listor går att förändra värdena på
+frukter[2] = "granatäpple"
+#print(frukter)
 
 # Det kan finnas dubletter i listor
 
 # Det går att spara olika datatyper i en lista
+blandad_lista = ["Felicia", 24, 1.7, True]
 
 # Vi kan loopa igenom en lista
+#for element in blandad_lista:
+ #   print(element)
 
 # Vi kan plocka ut längden på en lista
+#print(len(blandad_lista))
 
 # Metoder för listor: https://www.w3schools.com/python/python_lists_methods.asp
 
 # sort(), append(), pop(), remove(), reverse()
 
+tal_lista = [1,6,4,8,2,5,7,3,2]
+#print(tal_lista)
+sorterad_lista = tal_lista.copy()
+sorterad_lista.sort()
+#print(tal_lista)
+#print(sorterad_lista)
+
+tal_lista.append(5)
+
+
+
+
 #################################################################################################
 
 # Tuples
+
+bokstäver = ("a","o","u","å")
 
 # Som listor, men de går inte att ändra
 
@@ -34,7 +57,11 @@
 
 # Sets
 
+matvaror = {"tacobröd","paprika","mjölk","paprika","paprika"}
+
 # Ingen ordning, tillåter inte dubletter
+
+print(matvaror)
 
 # Går att lägga till och ta bort element, men inte byta ut eller ändra ett specifikt element
 
