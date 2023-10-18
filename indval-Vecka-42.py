3e18421bbaea1d0611863c160ba9cba88d521bcb
Felicia Rosenberg <felicia.rosenberg@tabyenskilda.se>
2023-10-18 14:06:27 +0200

    uppdaterat anteckningar

--- indval-Vecka-42.py
+++ indval-Vecka-42.py
@@ -2,11 +2,19 @@
 
 # Skrivs med {} och har par av nyckel och värde
 
+ålder = {"Felicia":24, "Victor":25, "Frida":24, "Max":15}
+
 # Ordnade, går att ändra, tillåter inte dubletter (inte samma nyckel)
+print(ålder["Felicia"])
+
+ålder["Felicia"]=25
+print(ålder["Felicia"])
 
 # Skriva ut ett värde från en nyckel
 
 
 # Kan innehålla olika datatyper
+
+favoriter = {"Victor":["matlagning","hockey"],"Frida":["Sy","sticka"]}
 
 # Metoder för dictionary: https://www.w3schools.com/python/python_dictionaries_methods.asp
