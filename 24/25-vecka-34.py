1b7aa76bcfbe4ce247078944898db3345afced7d
Felicia Rosenberg <felicia.rosenberg@tabyenskilda.se>
2024-08-19 10:09:28 +0200

    Förberett första genomgången, eventuellt hinner vi inte hela.

---
+++ 24/25-vecka-34.py
@@ -0,0 +1,95 @@
+# Kommentarer
+
+# Skrivs med # innan. Bra att kommentera sitt program så att man kommer ihåg vad man har gjort.
+# Högst upp i varje fil skriver ni ert namn, vad filen är till för och senast ni ändrade den
+
+
+# Hello world
+# Traditionellt första program
+#print används för att skriva ut någonting
+
+# Variabler
+
+# Vad kan jag döpa min variabel till?
+#   siffror, bokstäver, understreck
+#   understreck eller stor bokstav
+#   något namn som är beskrivande
+#   case sensitive
+#   vissa ord är "förbjudna"
+
+
+#Vanligare att skriva variabler med små bokstäver
+
+# Python förstår själv vilken typ variabeln har, man behöver oftast inte definiera det själv.
+
+
+# Datatyper
+
+#   String
+# en string är text, skrivs med "" runt
+
+
+#   Int (heltal)
+
+
+#   Float (decimaltal, kom ihåg att använda . istället för ,)
+
+
+#   Boolean (är antingen sann eller falsk)
+
+
+
+# Input
+#inputen blir automatiskt en string, 
+# om det ska vara t.ex. en int behöver vi "casta" den till int, det vill säga ändra explicit.
+
+
+
+# om vi har + måste vi göra om ökad_ålder till en string
+# vi kan också skriva , mellan två saker att printa, då läggs ett mellanslag automatiskt till
+
+
+# Det är skillnad vad som händer med + beroende på om man jobbar med int eller string
+
+
+
+# två olika sätt att lägga till 23 till ett heltal
+
+
+
+#print(tal_addera)
+ 
+
+
+# Ta reda på en typ för en variabel
+
+
+
+# Räkneoperationer
+
+#   +, -, *, /, //, %, **
+
+# vi kan addera både heltal och decimaltal
+
+
+
+# vi kan subtrahera med både heltal och decimaltal
+
+
+
+# vi kan multiplicera med både heltal och decimaltal
+
+
+
+# vi kan dividera som vanligt med / Då blir det ett decimaltal om det inte går jämnt ut.
+
+
+# vi kan göra heltalsdivision, då struntar programmet i decimaler. OBS: programmet avrundar inte
+
+# vi kan få ut resten vid en division. T.ex. blir 10%3 1 eftersom 3 får plats 3 gånger i 10, och det blir 1 över.
+
+
+# vi kan höja upp tal i något
+
+# Importera random
+
