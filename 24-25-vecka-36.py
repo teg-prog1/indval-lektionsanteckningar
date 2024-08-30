3547b9dce247b4f70e4e07baab6ae07e6252036b
Felicia Rosenberg <felicia.rosenberg@tabyenskilda.se>
2024-08-30 12:19:47 +0200

    Förberett genomgång v.36

---
+++ 24-25-vecka-36.py
@@ -0,0 +1,51 @@
+# if, else, elif
+
+# if, else och elif kan användas för att programmet ska göra olika saker beroende på villkor
+
+# om vi till exempel vill skriva ut olika saker beroende på hur stort ett tal är
+
+
+
+# För att markera att något är "inom" if-satsen eller inom elif eller inom else används indenteringar
+    # denna rad är exempelvis indenterad jämfört med raden ovan
+
+# jämförelser kan göras exempelvis med dessa:
+# <=, <, >, >=, ==, !=
+
+# mindre än eller lika med skrivs med <=, och större än eller lika med tvärtom med >=
+
+# att kolla om något inte är lika med görs med ! innan =
+
+
+# and, or, not
+# vi kan kolla två saker samtidigt
+
+# vi kan ha flera elif om vi vill
+
+
+
+# nästlade if-satser
+# vi kan också ha if-satser i varandra, så kallade nästlade if-satser
+
+
+
+
+#########################################################################################################
+
+
+# while-loop
+# kan användas när man vill köra en kod flera gånger tills något stoppar den
+# control c
+
+
+
+# for-loop
+# kan användas när man vill köra en kod ett visst antal gånger, eller loopa igenom en sak (till exempel en sträng)
+
+
+
+# for x in "apple"
+
+
+# nästlade for-loopar
+# vi kan även ha loopar i loopar, testa själv och se vad som skrivs ut
