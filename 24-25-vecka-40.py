a7a4bef91e5e60ff2447608241d1e95c31f6c06d
Felicia Rosenberg <felicia.rosenberg@tabyenskilda.se>
2024-09-27 13:27:50 +0200

    förberett anteckningar vecka 40

---
+++ 24-25-vecka-40.py
@@ -0,0 +1,25 @@
+############## DICTIONARY #############
+
+# Skrivs med {} och har par av nyckel och värde
+
+
+# Ordnade, går att ändra, tillåter inte dubletter (inte samma nyckel)
+
+
+# Skriva ut ett värde från en nyckel
+
+
+# Kan innehålla olika datatyper
+
+
+# Metoder för dictionary: https://www.w3schools.com/python/python_dictionaries_methods.asp
+
+
+
+
+############## FÅNGA FEL #############
+# fånga när programmet kraschar eller när vi själva vill fånga något fel som inte är en krasch
+
+# try/except
+
+# raise error
