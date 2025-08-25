464879eb247b88b1e8d84c8ba87517bbdced17d1
Felicia Rosenberg <felicia.rosenberg@tabyenskilda.se>
2025-08-25 13:11:08 +0200

    förberett vecka 35

---
+++ vecka-35-introduktion.py
@@ -0,0 +1,90 @@
+### UPPSTART OCH MATEMATISKA OPERATIONER ###
+
+# print
+
+# +, -, *, /
+
+
+# modulo, %
+
+# heltalsdivision, //
+# tar bort decimalerna, avrundar INTE
+
+
+# upphöjt i, **
+
+### HELTAL OCH FLYTTAL ###
+
+# heltal = integer (int)
+# går snabbare att räkna med
+# representeras med binära tal
+
+
+# flyttal = float
+# decimaltal, med punkt eftersom att Python är amerikanskt
+
+
+# går att blanda heltal och flyttal
+
+
+### VARIABLER ###
+
+# Refererar till något i datorns minne, som en låda
+# Tilldelar ett värde till variabeln
+
+# Vill ha beskrivande variabelnamn, till skillnad från matematiken
+
+# Vissa namn är förbjudna
+# Får ej börja med siffror, innehålla vissa specialtecken eller vara ett reserverat ord
+
+
+# Python klarar å, ä och ö
+
+# Håll er till ett språk, engelska eller svenska
+
+# Håll er till en stil, snake_case eller camelCase
+
+# Omvandla mellan flyttal och heltal
+
+# Ändra värde på variabel (+= osv)
+
+# Python är case-sensitive
+
+# Dynamiskt typat och statiskt typat
+# Python är dynamiskt typat
+# dvs vi behöver inte sätta en datatyp direkt
+
+
+
+### STRÄNGAR ###
+
+# En datatyp
+
+# Strängar som tal och tal som strängar
+
+# Starkt typat: kan inte blanda datatyper i beräkningar
+# Svagt typat: kan blanda datatyper i beräkningar
+# Python är starkt typat
+# Jämför med dynamiskt typat och statiskt typat
+
+#summa = ålder + antal_husdjur #FUNKAR EJ
+
+
+# Slå ihop strängar med + (konkatenera) och str()
+
+
+### UTSKRIFTER ###
+
+# Skriva ut flera saker på två sätt
+
+# Skriva ut radbrytningar, citattecken och liknande
+
+# Skriva ut med formatering
+
+### INMATNINGAR ###
+
+# input()
+
+
+# Omvandla input till tal
+
