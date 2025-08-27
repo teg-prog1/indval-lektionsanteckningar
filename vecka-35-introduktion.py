4e544d13b6ec5284f2d14c712d2bd0c3027822c7
Felicia Rosenberg <felicia.rosenberg@tabyenskilda.se>
2025-08-27 08:23:12 +0200

    fina anteckningar vecka 35

--- vecka-35-introduktion.py
+++ vecka-35-introduktion.py
@@ -1,26 +1,30 @@
 ### UPPSTART OCH MATEMATISKA OPERATIONER ###
 
 # print
-print("Hello world!")
+print("Hello world!") 
+# viktigt att skriva exakt såhär, inget mellanslag mellan print och parentes
 
 # +, -, *, /
-#print(4+7)
-#print(5-1)
-#print(3*4)
-#print(9/3)
+# De vanligaste räknesätten är likadana som i matematik
+# De fungerar bra på både flyttal och heltal
+print(4+7)
+print(5-1)
+print(3*4)
+print(9/3)
 
 # modulo, %
-#print(10%3)
-#print(76125743%3)
+# ger resten vid en division
+print(10%3) # ger resten vid division av 10 med 3, dvs det blir 1
+print(76125743%3) # oavsett tal kommer resten vid division med 3 alltid att bli 0, 1 eller 2
 
 # heltalsdivision, //
 # tar bort decimalerna, avrundar INTE
-#print(10//3)
-#print(2//3)
+print(10//3)
+print(2//3)
 
 
 # upphöjt i, **
-#print(9**2)
+print(9**2)
 
 ### HELTAL OCH FLYTTAL ###
 
@@ -34,9 +38,10 @@ print("Hello world!")
 
 
 
-# går att blanda heltal och flyttal
-#print(0.7+1.3)
-#print(8/4)
+# går att blanda heltal och flyttal i beräkningar
+# det som händer då är att Python helt enkelt tolkar heltalet som ett flyttal
+print(0.7+1.3)
+print(8/4) # en division ger alltid ett flyttal
 
 
 ### VARIABLER ###
@@ -63,37 +68,39 @@ barn_ålder = 5.5
 
 # Omvandla mellan flyttal och heltal (cast)
 float_syster_ålder = float(syster_ålder)
-#print(float_syster_ålder)
+print(float_syster_ålder)
 int_barn_ålder = int(barn_ålder)
-#print(int_barn_ålder)
+print(int_barn_ålder)
 
-# Ändra värde på variabel (+= osv)
+# Ändra värde på variabel
 antal_rum = 5
-antal_rum = antal_rum + 1
-antal_rum += 1
+antal_rum = antal_rum + 1 # vi kan lägga till 1 till antal_rum såhär
+antal_rum += 1 # vi kan även göra såhär
 
-# Python är case-sensitive
+# Python är case-sensitive, dvs vi Print är inte samma som print
 
 # Dynamiskt typat och statiskt typat
 # Python är dynamiskt typat
-# dvs vi behöver inte sätta en datatyp direkt
+# dvs vi behöver inte sätta en datatyp direkt, Python löser det när programmet körs
 
 
 
 ### STRÄNGAR ###
 
-# En datatyp
+# En datatyp, är vanlig text
 namn = "Felicia Hultén Mattsson"
 
 # Strängar som tal och tal som strängar
 antal_kurser = 5
-str_antal_kurser = str(antal_kurser)
-#print(antal_kurser+antal_kurser)
-#print(str_antal_kurser+str_antal_kurser)
+str_antal_kurser = str(antal_kurser) # vi kan göra om heltal till sträng
+
+# om vi lägger ihop strängar blir det annorlunda än om vi lägger ihop heltal
+print(antal_kurser+antal_kurser)
+print(str_antal_kurser+str_antal_kurser)
 
 klassrum = "242"
-int_klassrum = int(klassrum)
-#print(int_klassrum)
+int_klassrum = int(klassrum) # vi kan göra om strängar till heltal om de går att göra till heltal
+print(int_klassrum)
 
 # Starkt typat: kan inte blanda datatyper i beräkningar
 # Svagt typat: kan blanda datatyper i beräkningar
@@ -104,27 +111,27 @@ antal_hundar = "2"
 #summa = antal_katter + antal_hundar #FUNKAR EJ
 
 
-# Slå ihop strängar med + (konkatenera) och str()
+# Slå ihop strängar med + (konkatenera)
 syster_namn = "Frida"
-sammanlagda_namn = namn  + syster_namn
+sammanlagda_namn = namn  + syster_namn # att slå ihop strängar kallas konkatenera
 #print(sammanlagda_namn)
 
 
 ### UTSKRIFTER ###
 
 # Skriva ut flera saker på två sätt
-#print("Hej", namn)
-#print("Hej "+ namn)
+print("Hej", namn) # vi kan göra med kommatecken
+print("Hej "+ namn) # vi kan göra med +, men bara om allt är strängar
 
 # Skriva ut radbrytningar, citattecken och liknande
-#print("\"\nHallå\tdär!")
+print("\"\nHallå\tdär!") # backslash används för att skriva ut vissa specialtecken
 
-# Skriva ut med formatering
-#print(f"Hej där {namn} och {syster_namn}, hur mår ni idag?")
+# Skriva ut med formatering, för att skriva ut variabel inne i en sträng
+print(f"Hej där {namn} och {syster_namn}, hur mår ni idag?")
 
 ### INMATNINGAR ###
 
-# input()
+# input(), för att ta in saker från användaren
 print("Vad är ditt intresse?")
 intresse = input()
 
@@ -134,6 +141,7 @@ print(f"Vad kul att du tycker om att {intresse} och {intresse_två}")
 
 
 # Omvandla input till tal
+# Input resulterar alltid i sträng, om det ska vara ett tal så måste vi casta det
 antal_syskon = int(input("Hur många syskon har du? "))
 print(antal_syskon+9)
 
