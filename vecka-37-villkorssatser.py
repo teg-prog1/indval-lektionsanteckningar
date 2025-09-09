d149293a98aab07871a93883b98516afe9b560c6
Felicia Rosenberg <felicia.rosenberg@tabyenskilda.se>
2025-09-09 08:25:26 +0200

    anteckningar vecka 37 förberedda

---
+++ vecka-37-villkorssatser.py
@@ -0,0 +1,50 @@
+# Radbrytningar i program
+
+# Kommentarer, vad är en bra kommentar
+
+# Hur ska en uppgift se ut när ni har gjort den?
+
+
+# Felmeddelanden
+# Exempel:
+#f = open("demofile.txt")
+#print(f.read())
+# Lär er att läsa och tolka felmeddelanden, typ av fel, vilken rad
+
+# Tips om w3schools, länk finns i lektionsloggen
+
+# Övning felmeddelanden
+
+# if
+# indentering
+
+
+# jämförelser (==,<, >, <=, >=, !=)
+
+
+# else
+# elif
+
+
+
+# Kan man inte ha bara massa if istället för elif?
+ 
+
+
+# pass
+
+
+# booleans
+
+
+# bool == True
+
+# jämförelser blir booleans
+
+# and, or, not (inte xor)
+# prioriteringsregler med and/or
+
+
+
+# nästlade if-else, vi kan ha if/elif/else inuti andra if/elif/else
+
