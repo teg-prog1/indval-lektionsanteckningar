ea6852b000b82b45ccdab7f4f6ce4f68a5876b14
Felicia Rosenberg <felicia.rosenberg@tabyenskilda.se>
2025-09-15 12:51:31 +0200

    förberett anteckningar vecka 38

---
+++ vecka-38-loopar.py
@@ -0,0 +1,27 @@
+# for-loopar
+# range (kolla på w3schools)
+
+
+# while-loopar
+# avbryta oändliga loopar (tryck ctrl C)
+
+
+#while programmet_kör:
+    #print("Hej!")
+
+# allt som går att göra med for-loop går att göra med while-loop, men inte tvärtom
+
+
+
+# while-loop som "spelloop"
+
+
+# break, kan stoppa en loop
+
+
+# nästlade loopar
+
+
+# Importera random
+
+# Slumpa heltal
