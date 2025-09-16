239a2acd069bc9759250f3f973bc72fecd447986
Felicia Rosenberg <felicia.rosenberg@tabyenskilda.se>
2025-09-16 15:12:25 +0200

    halvfärdiga anteckningar vecka 38

--- vecka-38-loopar.py
+++ vecka-38-loopar.py
@@ -1,20 +1,38 @@
 # for-loopar
 # range (kolla på w3schools)
+#for i in range(10):
+    #print(i)
 
+namn = "Felicia"
+#for bokstav in namn:
+    #print(bokstav)
 
 # while-loopar
+kör_program = True
+
+#while kör_program:
+    #print("hej")
+
+
 # avbryta oändliga loopar (tryck ctrl C)
 
 
-#while programmet_kör:
-    #print("Hej!")
-
 # allt som går att göra med for-loop går att göra med while-loop, men inte tvärtom
 
+tal = 0
 
+#while tal < 10:
+    #print(tal)
+    #tal += 1
 
 # while-loop som "spelloop"
-
+"""
+inte_rätt_svar = True
+while inte_rätt_svar:
+    svar = input("Vad är 15*2? ")
+    if svar == "30":
+        inte_rätt_svar = False
+"""
 
 # break, kan stoppa en loop
 
@@ -23,5 +41,9 @@
 
 
 # Importera random
+import random
+
+slumptal = random.randint(1,5)
+print(slumptal)
 
 # Slumpa heltal
