3fff8e1a269a1c2d3c3b2c78645a6538904911ed
Felicia Rosenberg <felicia.rosenberg@tabyenskilda.se>
2025-09-18 14:15:49 +0200

    förberett anteckningar vecka 39

---
+++ vecka-39-strängar.py
@@ -0,0 +1,56 @@
+# OLIKA CITATIONSTECKEN
+
+# vi kan ha vanliga dubbla citattecken
+
+# vi kan ha enkla (samma tangent som *)
+
+# Vi kan också ha trippla (tre stycken enkla)
+# Då kan man ha radbrytningarr och citattecken inuti strängen
+
+
+# Trippla citattecken kan också användas som kommentarer
+'''Det här blir som en kommentar'''
+
+# STRÄNGAR ÄR ITERERBARA
+# De är lagrade som en följd av tecken i minnet
+# vi börjar räkna från 0, detta kommer att skriva ut första tecknet
+
+# Vi kan loopa igenom en sträng
+
+# Vi kan kolla om bokstäver finns i strängen
+
+
+#IMMUTABLE
+# Strängar i python är immutable (oföränderliga)
+# Det betyder att vi inte kan ändra innehållet på följande sätt
+#namn[0] = "G" # gör inte att strängen blir Gelicia istället, istället blir det ett felmeddelande
+    
+# JÄMFÖRA STRÄNGAR
+# Vi kan jämföra strängar precis som tal med == och !=
+# == kollar om strängarna är exakt lika
+
+
+# != kollar om de är olika
+
+
+# BITAR AV STRÄNGAR
+# Vi kan ta fram bitar av en sträng (kallas för att slicea (engelska: slice))
+
+
+# STRÄNGMETODER
+# Vi kan göra massa saker med strängar, det finns en lista på w3Schools över strängmetoder
+# https://www.w3schools.com/python/python_strings_methods.asp
+
+# Kolla längden på en sträng
+
+
+#även mellanslag och andra specialtecken räknas
+
+# räkna förekomsten av en delsträng
+# count()
+
+# göra så att bokstäver är stora/små
+# upper()
+# lower()
+
+# Finns massa fler metoder, ni får själva testa olika i uppgifterna
