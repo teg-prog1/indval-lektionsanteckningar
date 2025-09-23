2df2197d94ac49566424981a50d16f17df08b6f9
Felicia Rosenberg <felicia.rosenberg@tabyenskilda.se>
2025-09-23 15:49:15 +0200

    fördiga antekcningar vecka 39

--- vecka-39-strängar.py
+++ vecka-39-strängar.py
@@ -5,8 +5,10 @@
 # vi kan ha enkla (samma tangent som *)
 
 # Vi kan också ha trippla (tre stycken enkla)
-# Då kan man ha radbrytningarr och citattecken inuti strängen
-
+# Då kan man ha radbrytningar och citattecken inuti strängen
+'''tal = 3
+if tal%5 == 0:
+    print("Woho!")'''
 
 # Trippla citattecken kan också användas som kommentarer
 '''Det här blir som en kommentar'''
@@ -14,10 +16,17 @@
 # STRÄNGAR ÄR ITERERBARA
 # De är lagrade som en följd av tecken i minnet
 # vi börjar räkna från 0, detta kommer att skriva ut första tecknet
+namn = "Felicia"
+print(namn[0])
+print(namn[3])
 
 # Vi kan loopa igenom en sträng
+for tecken in namn:
+    print(tecken)
 
 # Vi kan kolla om bokstäver finns i strängen
+if "c" in namn:
+    print("Det finns ett c!")
 
 
 #IMMUTABLE
@@ -28,13 +37,19 @@
 # JÄMFÖRA STRÄNGAR
 # Vi kan jämföra strängar precis som tal med == och !=
 # == kollar om strängarna är exakt lika
-
-
 # != kollar om de är olika
+
+if "Felicia" != "felicia":
+    print("Hallå!")
+
 
 
 # BITAR AV STRÄNGAR
 # Vi kan ta fram bitar av en sträng (kallas för att slicea (engelska: slice))
+lång_sträng = "Hej Felicia, hoppas du mår bra idag!"
+print(lång_sträng[1:12:2]) # börjar på index 1 och slutar på 12 (men tar inte med 12),
+                            # den sista 2:an anger att vi ska gå varannat tecken
+print(lång_sträng[::-1]) # börjar på 0 och går genom hela strängen, går med steg -1, dvs baklänges
 
 
 # STRÄNGMETODER
@@ -42,15 +57,17 @@
 # https://www.w3schools.com/python/python_strings_methods.asp
 
 # Kolla längden på en sträng
-
+print(len(namn))
 
 #även mellanslag och andra specialtecken räknas
 
 # räkna förekomsten av en delsträng
 # count()
+print(namn.count("i"))
 
 # göra så att bokstäver är stora/små
 # upper()
 # lower()
+print(namn.upper())
 
 # Finns massa fler metoder, ni får själva testa olika i uppgifterna
