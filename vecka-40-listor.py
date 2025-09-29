851ac03299f5b28629a8f16c369ba0498127114e
Felicia Rosenberg <felicia.rosenberg@tabyenskilda.se>
2025-09-29 11:34:26 +0200

    anteckningar vecka 40 förberedda

---
+++ vecka-40-listor.py
@@ -0,0 +1,63 @@
+
+# fyra datatyper hittills, int, float, bool, string
+# string är itererbar
+
+
+# lista, element
+
+
+# ordnad
+#index
+
+# går att förändra
+
+
+# olika datatyper i listor
+
+# loopa igenom lista med for-loop
+
+
+# slicea listor
+#print(frukt_lista[1:4:2])
+
+# listomfattningar (list comprehensions)
+#newlist = [tal for tal in range(0,85) if tal%2==0]
+
+# Listmetoder ###
+# len()
+
+# append()
+
+
+# min()
+
+# sort() och sorted()
+
+
+# andra metoder på w3shools
+
+# lista med listor, matris
+
+
+# tipplar/ tuples
+
+# oföränderlig
+
+# ordnad precis som listor
+
+# packa in och packa upp tipplar
+
+#######################################################################################################################
+
+# Referenser i Python
+
+# Skapa ett nytt variabelnamn som refererar till samma lista
+# Kopiera en lista
+
+# kopiera med for-loop
+
+# kopiera med listomfattning
+
+# kopiera med slice (mest effektivt)
+
+# Platser i minnet som inget refereras till längre tas omhand genom Pythons garbage collect
